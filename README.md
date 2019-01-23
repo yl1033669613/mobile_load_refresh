d60f61dabae77cc25a6b7856d2a0edf9415f8bb9
unknown <1033669613@qq.com>
2019-01-23 17:46:17 +0800

    重构

--- README.md
+++ README.md
@@ -1,33 +1,30 @@
 # mobile_load_refresh
 一个下拉刷新上拉加载的`移动端`原生js插件,不依赖任何额外的js库，可自行设计下拉刷新上拉加载提示，以及loading的样式，5kb的大小简单易用。
 
-可用的 [demo](https://yl1033669613.github.io/mobile_load_refresh/dist/index.html) （pc端查看时请使用浏览器的移动端模拟功能。）
+可查看的 [demo](https://yl1033669613.github.io/mobile_load_refresh/dist/index.html) （pc端查看时请使用浏览器的移动端模拟功能。）
 
 ## 用法
-1. html部分需要一个滚动容器，通常为一个`div`(不能是body或者html元素)，必须设置容器的高度（`scrollEleHeight`）。
+1. html部分需要一个容器，通常为一个`div`(不能是body或者html元素)，必须设置容器。
 
 ```
-<div class="pl-container"> <!-- 滚动容器 -->
-	...
-</div>
-```
-如果启用了下拉刷新（参数中设置了下拉刷新回调函数），则需要一个下拉刷新的loading。
+<!-- 必需设置容器高度  默认300px-->
+<div class="pl-container" style="height: 100vh;">
+    <!-- 如果需要下拉刷新loading 请在这里添加以下一项 -->
+<div class="pl-down">刷新中...</div>
 
-```
-<div class="pl-container">
-	<p class="pl-down"><img src="./images/loading.png" class="loading-ico" alt=""></p>
-	...
+<div class="pl-wrapper">
+    
+    <!-- <div class="content">
+        列表代码.....
+    </div> -->
+    
+    <!-- 如果需要上拉加载loading 请在这里添加以下三项-->
+    <div class="pl-up-before">上拉加载更多</div>
+    <div class="pl-up-in">加载中...</div>
+    <div class="pl-up-end">没有更多了</div>
 </div>
-```
-如果启用了上拉加载（参数中设置了上拉加载回调函数），则需要三个上拉加载的提示。
+</div>
 
-```
-<div class="pl-container">
-	...
-	<p class="pl-up-before">上拉加载更多</p> <!-- 上拉加载前 -->
-	<p class="pl-up-in"><img src="./images/loading.png" class="loading-ico" alt="">加载中...</p> <!-- 上拉加载中 -->
-	<p class="pl-up-end">没有更多了</p> <!-- 所有数据加载完毕 -->
-</div>
 ```
 
 2. js/css部分下载`pullloading.js`或者`pullloading.min.js`,以及`pullloading.css`在html中引用
@@ -39,11 +36,12 @@
 
 ```javascript
 var pullLoading = new PullLoading({
-	container: '.pl-container', //下拉刷新容器选择器
-    pullUp: function(pl) { //上拉加载回调函数参数是插件实例本身
+	container: '.pl-container', // String 下拉刷新容器选择器 (必需)
+    threshold: 80, // Number default 80 (可选)
+    pullUp: function(pl) { // 上拉加载回调函数参数是插件实例本身 (可选)
     	...
     },
-    pullDown: function(pl) { //下拉刷新回调函数参数是插件实例本身
+    pullDown: function(pl) { // 下拉刷新回调函数参数是插件实例本身 (可选)
         ...
     }
 });
@@ -56,7 +54,7 @@ var pullLoading = new PullLoading({
 
 `pullLoading.pullUpEnd(bool)` 用于设置上拉加载是否加载完所有数据，有一个参数。如果加载完所有数据则传`true`否则`false`. 
 
-具体[示例代码](https://github.com/yl1033669613/mobile_load_refresh/blob/master/dist/index.html)
+可用[Demo](https://github.com/yl1033669613/mobile_load_refresh/blob/master/dist/index.html)
 
 ## demo效果图
 ![图片名称](https://github.com/yl1033669613/mobile_load_refresh/blob/master/demoGif.gif)
--- dist/lib/pullloading.js
+++ dist/lib/pullloading.js
@@ -1,6 +1,35 @@
 ;
 (function(win, undefined) {
     "use strict"
+    var PULLWRAPPER = ' .pl-wrapper',
+        PULLDOWN = ' .pl-down',
+        PULLUPBEFORE = ' .pl-up-before',
+        PULLUPIN = ' .pl-up-in',
+        PULLUPEND = ' .pl-up-end';
+    var CSSTSF = '',
+        CSSTSI = '';
+
+    function _getCssCpe() { //获取系统css transform, transition兼容写法由于在移动端 只需要考虑webkit
+        var transform = '',
+            transition = '',
+            divStyle = document.createElement('div').style,
+            transformArr = ['transform', 'webkitTransform'],
+            transitionArr = ['transition', 'webkitTransition'],
+            i = 0,
+            len = transformArr.length;
+        for (; i < len; i++) {
+            if (transformArr[i] in divStyle && !transform) {
+                transform = transformArr[i];
+            };
+            if (transitionArr[i] in divStyle && !transition) {
+                transition = transitionArr[i];
+            }
+        };
+        return {
+            transform: transform,
+            transition: transition
+        }
+    };
     // 对象合并
     function _extend(o, n, override) {
         for (var key in n) {
@@ -25,31 +54,20 @@
     };
     //隐藏
     function _hide(ele) {
-        ele.style.display = 'none'
+        ele.style.display = 'none';
+        return ele
     };
     //显示
     function _show(ele) {
-        ele.style.display = 'block'
+        ele.style.display = 'block';
+        return ele
     };
-    //css
-    function _css(dom, styleObj) {
+    //set css
+    function _setCss(ele, styleObj) {
         for (var prop in styleObj) {
-            var attr = prop.replace(/[A-Z]/g, function(word) {
-                return '-' + word.toLowerCase();
-            });
-            if (attr.indexOf('webkit') != -1) {
-                attr = '-' + attr;
-            };
-            dom.style[attr] = styleObj[prop]
-        }
-    };
-    //滚动区域向上卷起的高度
-    function _getScrollTop(ele) {
-        return ele.scrollTop
-    };
-    //滚动区域总高度
-    function _getScrollHeight(ele) {
-        return ele.scrollHeight
+            ele.style[prop] = styleObj[prop]
+        };
+        return ele
     };
 
     function PullLoading(opt) {
@@ -58,13 +76,14 @@
     PullLoading.prototype = {
         constructor: PullLoading,
         _init: function(opt) { // 初始化函数
-            var defOpt = {
+            var defaultOption = {
+                threshold: 80,
                 container: '',
-                pullDown: null, //刷新完毕回调 cb
-                pullUp: null //加载完毕回调 cb
+                pullDown: null,
+                pullUp: null
             };
-            this.dte = 80;
 
+            this.containerEle = null;
             this.scrollEle = null;
             this.refreshEl = null;
             this.pullUpBeforeEl = null;
@@ -82,56 +101,63 @@
             this.pullDownY = 0;
 
             this.contClientHeight = 0;
-            this.option = _extend(defOpt, opt, true);
+            this.option = _extend(defaultOption, opt, true);
+
             if (this._checkOption()) {
-                _css(this.scrollEle, { overflow: 'scroll', webkitOverflowScrolling: 'touch' }); //设置容器必须css
-                this.contClientHeight = this.scrollEle.clientHeight; //滚动区域可视高度
-                //判断上拉下拉
-                if (this.isPullUp) {
+                _setCss(this.containerEle, { overflow: 'hidden' });
+                _setCss(this.scrollEle, { overflow: 'scroll', webkitOverflowScrolling: 'touch' });
+                this.contClientHeight = this.scrollEle.clientHeight; //保存滚动区域可视高度
+                if (this.isPullUp) { //判断存在上拉 并初始化视图
                     _show(this.pullUpBeforeEl);
                     _hide(this.pullUpInEl);
                     _hide(this.pullUpInEl)
                 };
-                if (this.isPullDown) {
+                if (this.isPullDown) { //判断存在下拉 并初始化视图
+                    _show(this.refreshEl);
                     this.refreshLoadingEleHeight = this.refreshEl.offsetHeight;
-                    _css(this.refreshEl, { transition: 'transform .1s', top: -this.refreshLoadingEleHeight + 'px' });
+                    var obj = {
+                        top: -this.refreshLoadingEleHeight + 'px'
+                    };
+                    obj[CSSTSI] = 'transform .1s';
+                    _setCss(this.refreshEl, obj)
                 };
                 //初始化事件
                 this._initEvent()
             }
         },
-        _checkOption: function() { // 验证参数
-            var opts = this.option;
-            this.scrollEle = _getDomInstanceByClass(opts.container);
-            if (!_isDOMElement(this.scrollEle)) {
-                throw new Error(opts.container + '实例未找到')
-            } else if (_testHTMLOrBODY(this.scrollEle)) {
-                throw new Error(opts.container + '不能是HTML或者BODY节点')
-            };
-            if ((typeof opts.pullDown !== 'function') && (typeof opts.pullUp !== 'function')) {
+        _checkOption: function() { // 验证
+            var opts = this.option,
+                cssCpe = _getCssCpe();
+            CSSTSF = cssCpe.transform;
+            CSSTSI = cssCpe.transition;
+            try {
+                if (!CSSTSF || !CSSTSI) throw new Error('浏览器不支持本插件');
+                this.containerEle = _getDomInstanceByClass(opts.container);
+                if (!_isDOMElement(this.containerEle)) {
+                    throw new Error(opts.container + ' 实例未找到')
+                } else if (_testHTMLOrBODY(this.containerEle)) {
+                    throw new Error(opts.container + ' 不能是HTML或者BODY节点')
+                } else {
+                    this.scrollEle = _getDomInstanceByClass(this.option.container + PULLWRAPPER)
+                };
+                if ((typeof opts.pullDown !== 'function') && (typeof opts.pullUp !== 'function')) return false;
+                if (typeof opts.pullDown === 'function') {
+                    this.refreshEl = _getDomInstanceByClass(opts.container + PULLDOWN);
+                    if (!_isDOMElement(this.refreshEl)) throw new Error('请添加下拉刷新loading');
+                    this.isPullDown = true
+                };
+                if (typeof opts.pullUp === 'function') {
+                    this.pullUpBeforeEl = _getDomInstanceByClass(opts.container + PULLUPBEFORE);
+                    this.pullUpInEl = _getDomInstanceByClass(opts.container + PULLUPIN);
+                    this.pullUpEndEl = _getDomInstanceByClass(opts.container + PULLUPEND);
+                    if (!_isDOMElement(this.pullUpBeforeEl)) throw new Error('请添加上拉加载前提示');
+                    if (!_isDOMElement(this.pullUpInEl)) throw new Error('请添加上拉加载中提示');
+                    if (!_isDOMElement(this.pullUpEndEl)) throw new Error('请添加上拉加载已加载全部内容提示');
+                    this.isPullUp = true
+                };
+            } catch (e) {
+                console.error(e)
                 return false
-            };
-            if (typeof opts.pullDown === 'function') {
-                this.refreshEl = _getDomInstanceByClass(opts.container + ' .pl-down');
-                if (!_isDOMElement(this.refreshEl)) {
-                    throw new Error('请添加下拉刷新loading')
-                };
-                this.isPullDown = true
-            };
-            if (typeof opts.pullUp === 'function') {
-                this.pullUpBeforeEl = _getDomInstanceByClass(opts.container + ' .pl-up-before');
-                this.pullUpInEl = _getDomInstanceByClass(opts.container + ' .pl-up-in');
-                this.pullUpEndEl = _getDomInstanceByClass(opts.container + ' .pl-up-end');
-                if (!_isDOMElement(this.pullUpBeforeEl)) {
-                    throw new Error('请添加上拉加载前提示')
-                };
-                if (!_isDOMElement(this.pullUpInEl)) {
-                    throw new Error('请添加上拉加载中提示')
-                };
-                if (!_isDOMElement(this.pullUpEndEl)) {
-                    throw new Error('请添加上拉加载已加载全部内容提示')
-                };
-                this.isPullUp = true
             };
             return true
         },
@@ -142,53 +168,61 @@
             this.scrollEle.addEventListener('scroll', this._scrollHandle.bind(this))
         },
         _touchstartHandle: function(e) {
-            this.startY = e.changedTouches[0].clientY;
-            this.currY = e.changedTouches[0].clientY;
+            this.startY = this.currY = e.changedTouches[0].clientY;
             this.pullDownY = 0
         },
         _touchmoveHandle: function(e) {
-            var touches = e.changedTouches[0];
-            if (_getScrollTop(this.scrollEle) == 0) { //当向上滚动的高度=0，或者说滚动位置处在最顶部 scrollTop = 0
-                if (_getScrollHeight(this.scrollEle) <= this.contClientHeight && this.isPullUp && touches.clientY - this.startY < 0) { //可滚动容器没有滚动条 阻止默认行为
+            var touchesY = e.changedTouches[0].clientY;
+            if (this.scrollEle.scrollTop == 0) { //当向上滚动的高度=0，或者说滚动位置处在最顶部 scrollTop = 0
+                if (this.isPullUp && this.scrollEle.scrollHeight <= this.contClientHeight && touchesY - this.startY < 0) { //可滚动容器没有滚动条 并且向上拉时 阻止默认行为
                     e.preventDefault()
                 };
-                if (this.isPullDown && touches.clientY - this.startY >= 0 && !this.pullUpLoading && !this.pullDownLoading) { //下拉的判断方向为由上往下 并且存在下拉刷新回调函数 且不在上拉加载当中时
-                    e.preventDefault(); //滚动到顶部时继续下拉阻止浏览器默认行为
-                    this.pullDownY = touches.clientY - this.currY + this.pullDownY;
-                    this.currY = touches.clientY;
-                    var _pullDownY = this.pullDownY / (this.dte / this.refreshLoadingEleHeight);
+                if (this.isPullDown && touchesY - this.startY >= 0 && !this.pullUpLoading && !this.pullDownLoading) { //下拉的判断方向为由上往下 并且存在下拉刷新回调函数 且不在上拉加载当中时
+                    e.preventDefault();
+                    this.pullDownY = touchesY - this.currY + this.pullDownY;
+                    this.currY = touchesY;
+                    var _pullDownY = this.pullDownY / (this.option.threshold / this.refreshLoadingEleHeight);
                     if (_pullDownY >= this.refreshLoadingEleHeight) {
                         _pullDownY = this.refreshLoadingEleHeight
                     };
-                    _css(this.refreshEl, { transition: 'transform 0s', transform: 'translate(0, ' + _pullDownY + 'px)' }) //设置loading提示dom位置使其跟随拖动有一个下拉的动作
+                    var obj = {};
+                    obj[CSSTSI] = 'transform 0s';
+                    obj[CSSTSF] = 'translate(0, ' + _pullDownY + 'px)';
+                    _setCss(this.refreshEl, obj)
                 }
             }
         },
         _touchendHandle: function(e) {
             if (this.pullUpLoading || this.pullDownLoading) return;
-            var touches = e.changedTouches[0];
-            if (_getScrollTop(this.scrollEle) == 0) {
-                if (touches.clientY - this.startY >= 0) {
-                    if (touches.clientY - this.startY >= this.dte) {
-                        _css(this.refreshEl, { transform: 'translate(0, ' + this.refreshLoadingEleHeight + 'px)' });
+            var touchesY = e.changedTouches[0].clientY;
+            if (this.scrollEle.scrollTop == 0) {
+                if (this.isPullDown) {
+                    if (touchesY - this.startY >= this.option.threshold) {
+                        var obj = {};
+                        obj[CSSTSF] = 'translate(0, ' + this.refreshLoadingEleHeight + 'px)';
+                        _setCss(this.refreshEl, obj);
                         this._pullDown()
                     } else { //下拉未拖动到阈值时恢复loading位置
-                        _css(this.refreshEl, { transition: 'transform .2s', transform: 'translate(0, 0)' })
+                        var obj = {};
+                        obj[CSSTSI] = 'transform .2s';
+                        obj[CSSTSF] = 'translate(0, 0)';
+                        _setCss(this.refreshEl, obj)
                     }
-                } else {
-                    if (_getScrollHeight(this.scrollEle) <= this.contClientHeight && touches.clientY - this.startY <= -this.dte) {
+                };
+                if (this.isPullUp) {
+                    if (this.scrollEle.scrollHeight <= this.contClientHeight && touchesY - this.startY <= -this.option.threshold) {
                         this._pullUp()
                     }
                 }
             }
         },
         _scrollHandle: function(e) { //当存在滚动条时，上拉加载以滚动到底部为触发条件
-            if (_getScrollTop(this.scrollEle) + this.contClientHeight == _getScrollHeight(this.scrollEle)) {
+            if (this.isPullUp && this.scrollEle.scrollTop + this.contClientHeight == this.scrollEle.scrollHeight) {
                 this._pullUp()
             }
         },
         _pullUp: function() {
-            if (this.isPullUp && !this.isPullUpEnd && !this.pullDownLoading && !this.pullUpLoading) {
+            if (!this.isPullUpEnd && !this.pullDownLoading && !this.pullUpLoading) {
                 this.pullUpLoading = true;
                 _hide(this.pullUpBeforeEl);
                 _show(this.pullUpInEl);
@@ -196,7 +230,7 @@
             }
         },
         _pullDown: function() {
-            if (this.isPullDown && !this.pullDownLoading && !this.pullUpLoading) {
+            if (!this.pullDownLoading && !this.pullUpLoading) {
                 this.pullDownLoading = true;
                 this.pullUpEnd(false);
                 this.option.pullDown && this.option.pullDown(this)
@@ -208,10 +242,14 @@
                 _hide(this.pullUpInEl)
             };
             if (this.isPullDown) {
-                _css(this.refreshEl, { transition: 'transform .2s', transform: 'translate(0, 0)' })
+                var obj = {};
+                obj[CSSTSI] = 'transform .2s';
+                obj[CSSTSF] = 'translate(0, 0)';
+                _setCss(this.refreshEl, obj)
             };
             this.pullUpLoading = false;
-            this.pullDownLoading = false
+            this.pullDownLoading = false;
+            return this
         },
         pullUpEnd: function(bool) { //设置上拉加载完所有数据时显示提示信息
             if (!this.isPullUp) return;
@@ -224,8 +262,10 @@
                 _show(this.pullUpBeforeEl);
                 _hide(this.pullUpInEl);
                 _hide(this.pullUpEndEl)
-            }
+            };
+            return this.isPullUpEnd
         }
     };
+
     !('PullLoading' in win) && (win.PullLoading = PullLoading)
 }(window))
